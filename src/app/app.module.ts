6a84a8606b8a029bc6db6dc7b38ff12ee46a4288
little-by <kotarzhang@outlook.com>
2017-12-05 00:41:06 +0800

    6.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import {HeroesComponent} from './heroes.component'
+import { HeroService } from './hero.service';
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+

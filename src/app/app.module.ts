90d8a12f12f84dbc8d34fd4c8c01359d3833a6e5
little-by <kotarzhang@outlook.com>
2017-12-06 22:59:23 +0800

    7.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { HeroService } from './hero.service';
 import { RouterModule }   from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpModule }    from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
     
   ],
--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -18,4 +18,6 @@ export class DashboardComponent implements OnInit {
         this.heroService.getHeroes()
           .then(heroes => this.heroes = heroes.slice(1, 5));
       }
+
+      
     }
